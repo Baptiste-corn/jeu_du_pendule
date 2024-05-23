f276a8440262b4f8398742cbd3a93ed4af79562b
Baptiste-corn <61588561+Baptiste-corn@users.noreply.github.com>
2024-05-24 00:16:31 +0200

    Jeu du pendue

--- main.py
+++ main.py
@@ -2,103 +2,137 @@
 
 from random import choice
 
+
 # On récupère le fichier pour les mots du pendu
-def ouverture_fichier () :
-    choix_du_fichier = 0 # initialisation de la variable
+def ouverture_fichier():
+    # l'utilisateur choisit le fichier qu'il veut, personnalisé ou par défaut
     choix_du_fichier = int(input(
         "Voulez-vous une banque de mot personnalisé ou par défaut ? \n1 : Personnalisée\n2 : Par défaut\n"))
     if choix_du_fichier == 1:
         fichier_utilisateur = input("veuillez chosir un fichier (seconde_liste.txt)")
         fichier = open(fichier_utilisateur, "r", encoding="utf-8")
     elif choix_du_fichier == 2:
-        fichier = open("mots_pendu.txt", "r",encoding="utf-8")
-        # with open("C:/Users/bapti/Desktop/ETS/Ete_2024/MGA802/projetPendu/mots_pendu.txt", "r") as fio:
-        #   fichier = fio.read().splitlines()
+        fichier_utilisateur = "mots_pendu.txt"
+        fichier = open(fichier_utilisateur, "r", encoding="utf-8")
     else:
         print("ouverture par défaut")
-        with open("C:/Users/bapti/Desktop/ETS/Ete_2024/MGA802/projetPendu/mots_pendu.txt", "r") as fio:
-            fichier = fio.read().splitlines()
+        fichier = open("mots_pendu.txt", "r", encoding="utf-8")
 
     return fichier
 
 
 def ecrire_fichier_dans_chaine(fichier):
+    # On ecrit le fichier dans une liste pour pouvoir manipuler les mots dans le script
     banque_de_mots = fichier.read().splitlines()
     return banque_de_mots
 
+
 def mot_aleatoire(banque_de_mots):
+    # On choisit un mot aléatoire que le joueur va devoir deviner
     mot_a_deviner = choice(banque_de_mots)
     return mot_a_deviner
 
-def supprimer_accent (mot_choisi):
+
+def supprimer_accent(mot_choisi):
+    # Cette fonction sert à supprimer les accents du mot choisit aléatoirement
+    # On declare une liste avec les accents présents dans la liste de mot
     liste_accent = ['à', 'é', 'è', 'ê', 'î', 'ô', 'ù', 'û']
+
+    # On déclare une seconde liste sans accent pour les remplacer
     liste_sans_accent = ['a', 'e', 'e', 'e', 'i', 'o', 'u', 'u']
+
+    # initialisation de la variable qui contiendra le mot sans les accents
     mot_sans_accent = ""
 
-    for i in range(len(mot_choisi)): # On parcourt le mot
-        if mot_choisi[i] in liste_accent : # Si une lettre du mot contient un accent present dans la liste_accent
+    for i in range(len(mot_choisi)):  # On parcourt le mot
+        if mot_choisi[i] in liste_accent:  # Si une lettre du mot contient un accent present dans la liste_accent
+
+            # On réécrit le mot sans les accents
             mot_sans_accent += liste_sans_accent[liste_accent.index(mot_choisi[i])]
-            # On concatene notre liste avec la lettre sans accent
-        else:
+
+        else:  # Sinon on recopie juste le mot
             mot_sans_accent += mot_choisi[i]
 
     return mot_sans_accent
 
-def affichage_mot (mot_sans_accent):
 
+def affichage_mot(mot_sans_accent):
+    # cette fonction sert a afficher le mot sous forme de tiret "_" pour aider visuellement le joueur
+    mot_cache = []
     for i in range(len(mot_sans_accent)):
         mot_cache = len(mot_sans_accent) * ['_']
 
     return mot_cache
 
-# Le mot est choisi aléatoirement et les accents sont retires
-
 
 # Debut de la boucle pour le jeu du pendu
-
-
 def jeu_pendu(mon_mot, mot_cache):
-
-    vie_du_joueur = 6
+    vie_du_joueur = 6  # Le joueur possède 6 vies au début de la partie
     compteur = 0  # Pour remplir la condition de victoire
+
+    # Cette liste est utilisée pour donner un indice au joueur lorqu'il n'a plus qu'une vie
+    liste_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                      's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
     print("Bienvenue au jeu du pendu, essayez de deviner le mot !")
     print(f'le mot contient {len(mon_mot)} lettres, bon courage ! ')
 
+    # Boucle de jeu
     while vie_du_joueur != 0:
-        print(f'Il vous reste {vie_du_joueur}')
-        print(mot_cache)
+        print(f'Il vous reste {vie_du_joueur} vie(s)')
+        print(mot_cache)  # On affiche le mot sous forme de tiret
         lettre = "null"
 
+        while len(lettre) != 1:  # On s'assure que l'utilisateur ne peut choisir qu'une seule lettre à la fois
+            lettre = input("\nveuillez choisir une lettre minuscule : ")
 
-        while len(lettre) != 1:
-            lettre = input("veuillez choisir une lettre minuscule : ")
+        if lettre in mon_mot:  # On affiche la lettre à la place du tiret si elle est présente dans le mot
 
-        if lettre in mon_mot:
-            del mot_cache[mon_mot.index(lettre)]
-            mot_cache.insert(mon_mot.index(lettre), lettre)
-            compteur += 1
-
-
+            # La boucle sert à afficher plusieurs fois la même lettre si le mot possède des doubloons
+            for i in range(len(mon_mot)):
+                if lettre == mon_mot[i]:
+                    del mot_cache[i]
+                    mot_cache.insert(i, lettre)
+                    compteur += 1
         else:
-            print("ce n'est pas la bonne lettre, vous perdez une vie")
+            print("ce n'est pas la bonne lettre, vous perdez une vie\n")
             vie_du_joueur -= 1
 
+            # Indice pour le joueur s'il n'a plus qu'une vie
+            if vie_du_joueur == 1:
+                for i in range(len(mon_mot)):
+                    if mon_mot[i] in liste_alphabet:
+                        liste_alphabet.remove(mon_mot[i])  # On supprime les lettres du mot présentes dans l'alphabet
+
+                indice = choice(liste_alphabet)
+                # On affiche une lettre qui n'est pas dans le mot
+                print(f'indice : la lettre suivante n est pas dans le mot : {indice}')
 
         # Condition de victoire
         if compteur == len(mot_cache):
             choix = 0
-            print ("Bravo, vous avez trouvé le mot !")
-            print(mon_mot)
+            print(f'Bravo, vous avez trouvé le mot {mon_mot}!')
+
             while choix != 1 or choix != 2:
-                choix = int(input("Voulez vous refaire une partie ? \n1 : Rejouer  \n2 : Arreter le programme\nchoix : "))
+                choix = int(
+                    input("Voulez vous refaire une partie ? \n1 : Rejouer  \n2 : Arreter le programme\nchoix : "))
                 if choix == 1:
-                    recommencer()
+                    recommencer()  # Fonction pour recommencer le jeu avec un nouveau mot
                 elif choix == 2:
                     print("Au revoir")
                     return 0
-
-
-
+        # Si le joueur a perdu toutes ses vies
+        elif vie_du_joueur == 0:
+            print(f'vous avez perdu\nLe mot était {mon_mot}')
+            choix = 0
+            while choix != 1 or choix != 2:
+                choix = int(
+                    input("Voulez vous refaire une partie ? \n1 : Rejouer  \n2 : Arreter le programme\nchoix : "))
+                if choix == 1:
+                    recommencer()  # Fonction pour recommencer le jeu avec un nouveau mot
+                elif choix == 2:
+                    print("Au revoir")
+                    return 0
 
 
 def initialisation():
@@ -106,18 +140,17 @@ def initialisation():
     liste_mots = ecrire_fichier_dans_chaine(fichier)
     mot_a_deviner = mot_aleatoire(liste_mots)
     mon_mot = supprimer_accent(mot_a_deviner)
-    print(mon_mot)
+    print(f'Pour aider a la correction, on donne le mot choisit aléatoirement : {mon_mot}')
     mot_cache = affichage_mot(mon_mot)
     jeu_pendu(mon_mot, mot_cache)
 
 
-def recommencer ():
-
+def recommencer():
     fichier = ouverture_fichier()
     liste_mots = ecrire_fichier_dans_chaine(fichier)
     mot_a_deviner = mot_aleatoire(liste_mots)
     mon_mot = supprimer_accent(mot_a_deviner)
-    print(mon_mot)
+    print(f'Pour aider a la correction, on donne le mot choisit aléatoirement : {mon_mot}\n')
     mot_cache = affichage_mot(mon_mot)
     jeu_pendu(mon_mot, mot_cache)
 
